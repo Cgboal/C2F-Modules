08ed24174270ef419a0486d4637b03736c977493
Cgboal <Cgboal521@gmail.com>
2018-04-19 01:36:09 +0100

    Forgot to increment setup.py version number

--- C2F-PingSweep/C2Wrapper.py
+++ C2F-PingSweep/C2Wrapper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from c2sdk import rest
-from netaddr import IPNetwork
+from netaddr import IPNetwork, iter_unique_ips
 import socket
 import fcntl
 import struct
+import subprocess
 
 iface = "eth0"
 netmask = socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])
@@ -12,4 +13,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 local_ip = s.getsockname()[0]
 s.close()
-print str(IPNetwork(local_ip+"/"+netmask).cidr)
+cidr = IPNetwork(local_ip+"/"+netmask).cidr
+print str(cidr)
+alive_hosts = []
+
+for ip in iter_unique_ips(cidr):
+    res = subprocess.call(['ping', '-c', '3', str(ip)])
+    if res == 0:
+        alive_hosts.append(ip)
+print alive_hosts
+#rest.post("PingSweepResult", {"subnet": (cidr), "hosts_alive": ",".join(alive_hosts)})str
